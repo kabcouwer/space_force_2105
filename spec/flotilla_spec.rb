aeb5fafc5419e5afa98bfa8b720bb7c93159149c
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-06-03 09:44:10 -0600

    Create add_personnel and recommend_personnel tests

--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -28,6 +28,71 @@ RSpec.describe Flotilla do
 
       expect(seventh_flotilla.ships).to eq([daedalus])
     end
+
+    it 'can add personnel' do
+      daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+      daedalus.add_requirement({astrophysics: 6})
+      daedalus.add_requirement({quantum_mechanics: 3})
+
+      seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+      seventh_flotilla.add_ship(daedalus)
+
+      kathy = Person.new('Kathy Chan', 10)
+      kathy.add_specialty(:astrophysics)
+      kathy.add_specialty(:quantum_mechanics)
+
+      polly = Person.new('Polly Parker', 8)
+      polly.add_specialty(:operations)
+      polly.add_specialty(:maintenance)
+
+      rover = Person.new('Rover Henriette', 1)
+      rover.add_specialty(:operations)
+      rover.add_specialty(:maintenance)
+
+      sampson = Person.new('Sampson Edwards', 7)
+      sampson.add_specialty(:astrophysics)
+      sampson.add_specialty(:quantum_mechanics)
+
+      seventh_flotilla.add_personnel(kathy)
+      seventh_flotilla.add_personnel(polly)
+      seventh_flotilla.add_personnel(rover)
+      seventh_flotilla.add_personnel(sampson)
+
+      expect(seventh_flotilla.personnel).to eq([kathy, polly, rover, sampson])
+    end
+
+    it 'can recommend personnel that match ships requirements' do
+      daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+      daedalus.add_requirement({astrophysics: 6})
+      daedalus.add_requirement({quantum_mechanics: 3})
+
+      seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+      seventh_flotilla.add_ship(daedalus)
+
+      kathy = Person.new('Kathy Chan', 10)
+      kathy.add_specialty(:astrophysics)
+      kathy.add_specialty(:quantum_mechanics)
+
+      polly = Person.new('Polly Parker', 8)
+      polly.add_specialty(:operations)
+      polly.add_specialty(:maintenance)
+
+      rover = Person.new('Rover Henriette', 1)
+      rover.add_specialty(:operations)
+      rover.add_specialty(:maintenance)
+
+      sampson = Person.new('Sampson Edwards', 7)
+      sampson.add_specialty(:astrophysics)
+      sampson.add_specialty(:quantum_mechanics)
+
+      seventh_flotilla.add_personnel(kathy)
+      seventh_flotilla.add_personnel(polly)
+      seventh_flotilla.add_personnel(rover)
+      seventh_flotilla.add_personnel(sampson)
+      
+      expect(seventh_flotilla.recommend_personnel).to eq([kathy, sampson])
+
+    end
   end
 
 end
