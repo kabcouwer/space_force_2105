cd68773074e6c1b948af02636237f7aaab24d04c
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-06-03 10:38:44 -0600

    Add recommend personnel method

--- lib/flotilla.rb
+++ lib/flotilla.rb
@@ -11,4 +11,23 @@ class Flotilla
   def add_ship(ship)
     @ships << ship
   end
+
+  def add_personnel(person)
+    @personnel << person
+  end
+
+  def recommend_personnel
+    answer = []
+    @ships.each do |ship|
+      ship.requirements.map do |requirement|
+        @personnel.find_all do |person|
+          # require 'pry'; binding.pry
+          if person.specialties.include?(requirement.keys.shift)
+            answer << person
+          end
+        end
+      end
+    end
+    answer.uniq
+  end
 end
