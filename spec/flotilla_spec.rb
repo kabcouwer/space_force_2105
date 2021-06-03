17d9a06f585ab9f36f58949a7800f3bc8cc35b91
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-06-03 10:52:18 -0600

    Create test for personnel by ship

--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -100,6 +100,48 @@ RSpec.describe Flotilla do
       expect(seventh_flotilla.recommend_personnel(odyssey)).to eq([polly])
 
     end
+
+    it 'can return a hash where keys are ships and values are an array of personnel that are qualified to be on said ship' do
+      daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+      daedalus.add_requirement({astrophysics: 6})
+      daedalus.add_requirement({quantum_mechanics: 3})
+
+      odyssey = Spacecraft.new({name: 'Odyssey', fuel: 300})
+      odyssey.add_requirement({operations: 6})
+      odyssey.add_requirement({maintenance: 3})
+
+      seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+
+      kathy = Person.new('Kathy Chan', 10)
+      kathy.add_specialty(:astrophysics)
+      kathy.add_specialty(:quantum_mechanics)
+
+      polly = Person.new('Polly Parker', 8)
+      polly.add_specialty(:operations)
+      polly.add_specialty(:maintenance)
+
+      rover = Person.new('Rover Henriette', 1)
+      rover.add_specialty(:operations)
+      rover.add_specialty(:maintenance)
+
+      sampson = Person.new('Sampson Edwards', 7)
+      sampson.add_specialty(:astrophysics)
+      sampson.add_specialty(:quantum_mechanics)
+
+      seventh_flotilla.add_personnel(kathy)
+      seventh_flotilla.add_personnel(polly)
+      seventh_flotilla.add_personnel(rover)
+      seventh_flotilla.add_personnel(sampson)
+
+      seventh_flotilla.add_ship(daedalus)
+      seventh_flotilla.add_ship(odyssey)
+
+
+      expect(seventh_flotilla.personnel_by_ship).to eq({
+        daedalus => [kathy, sampson],
+        odyssey => [polly]
+        })
+    end
   end
 
 end
